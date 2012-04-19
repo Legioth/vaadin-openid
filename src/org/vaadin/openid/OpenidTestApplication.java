f172b71fd75b5ccde8b490f042d7219430513f35
Leif Åstrand <legioth@gmail.com>
2012-04-19 22:26:52 +0300

    Initial version

---
+++ src/org/vaadin/openid/OpenidTestApplication.java
@@ -0,0 +1,76 @@
+package org.vaadin.openid;
+
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
+
+import org.vaadin.openid.OpenIdHandler.UserAttribute;
+
+import com.vaadin.Application;
+import com.vaadin.ui.HorizontalLayout;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Link;
+import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
+
+public class OpenidTestApplication extends Application {
+
+    @Override
+    public void init() {
+        VerticalLayout container = new VerticalLayout();
+        container.setSpacing(true);
+        container.setMargin(true);
+
+        Window mainWindow = new Window("OpenId test", container);
+        setMainWindow(mainWindow);
+
+        final OpenIdHandler openIdHandler = new OpenIdHandler(this);
+        openIdHandler.setRequestedAttributes(UserAttribute.values());
+
+        final HorizontalLayout linkHolder = new HorizontalLayout();
+        linkHolder.setSpacing(true);
+        linkHolder
+                .addComponent(createLoginLink(openIdHandler,
+                        "https://www.google.com/accounts/o8/id",
+                        "Log in using Google"));
+        linkHolder.addComponent(createLoginLink(openIdHandler,
+                "https://me.yahoo.com", "Log in using Yahoo"));
+
+        container.addComponent(linkHolder);
+
+        openIdHandler.addListener(new OpenIdHandler.OpenIdLoginListener() {
+            public void onLogin(String id, Map<UserAttribute, String> userInfo) {
+                Window window = getMainWindow();
+                window.removeComponent(linkHolder);
+                window.addComponent(new Label("Logged in identity: " + id));
+                Set<UserAttribute> missingFields = EnumSet
+                        .allOf(UserAttribute.class);
+                for (UserAttribute field : userInfo.keySet()) {
+                    window.addComponent(new Label(field + ": "
+                            + userInfo.get(field)));
+                    missingFields.remove(field);
+                }
+                for (UserAttribute registrationFields : missingFields) {
+                    window.addComponent(new Label(registrationFields
+                            + " not provided"));
+                }
+
+                openIdHandler.close();
+            }
+
+            public void onCancel() {
+                getMainWindow().removeComponent(linkHolder);
+                getMainWindow().addComponent(
+                        new Label("Too sad you didn't want to log in."));
+
+                openIdHandler.close();
+            }
+        });
+    }
+
+    private static Link createLoginLink(OpenIdHandler openIdHandler, String id,
+            String caption) {
+        return new Link(caption, openIdHandler.getLoginResource(id),
+                "openidLogin", 600, 400, Window.BORDER_NONE);
+    }
+}
